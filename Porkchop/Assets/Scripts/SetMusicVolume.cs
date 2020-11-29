5bb6d0e2389ff5d8f364dc69bf875204ebc463e8
Meta1710 <73332300+Meta1710@users.noreply.github.com>
2020-11-29 18:56:52 +0100

    Full working menu

---
+++ Porkchop/Assets/Scripts/SetMusicVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetMusicVolume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetBGMVolume (float volume)
+    {
+        audioMixer.SetFloat("BGMVolume", volume);
+        
+    }
+
+    public void SetSFXVolume(float volume)
+    { audioMixer.SetFloat("SFXVolume", volume); }
+}
