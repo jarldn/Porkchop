b2404a97b5c1b3a3dd39eef07b957773070e2f8f
Meta1710 <73332300+Meta1710@users.noreply.github.com>
2021-01-13 20:11:47 +0100

    Animation test on brushes

---
+++ Porkchop/Assets/Scripts/BrushAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushAnim : MonoBehaviour
+{
+    [SerializeField] private Animator AnimationController;
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AnimationController.SetBool("PlayBrush", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AnimationController.SetBool("PlayBrush", false);
+        }
+    }
+
+}
