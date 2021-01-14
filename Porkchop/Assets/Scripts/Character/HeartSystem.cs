f7f5fe074e03aba2b3fe34a883ea23addf277e87
InoriSaika <64062876+InoriSaika@users.noreply.github.com>
2021-01-14 17:44:42 +0100

    health

---
+++ Porkchop/Assets/Scripts/Character/HeartSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class HeartSystem : MonoBehaviour
+{
+
+    public GameObject[] hearts;
+    public int vidas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vidas < 1)
+        {
+            Destroy(hearts[0].gameObject);
+        }
+        else if (vidas < 2)
+        {
+            Destroy(hearts[1].gameObject);
+        } 
+        else if (vidas < 3)
+        {
+            Destroy(hearts[1].gameObject);
+        }
+    }
+
+    public void fakeDamage(int d)
+    {
+        vidas -= d;
+    }
+}
