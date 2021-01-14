38cd992781de2a8e079f34c88c0aaf882657e967
jarldn <59234699+jarldn@users.noreply.github.com>
2021-01-14 13:04:25 +0100

    test

---
+++ Porkchop/Assets/Materials/Shader/includes/Lighting1.hlsl
@@ -0,0 +1,31 @@
+//    Copyright (C) 2020 NedMakesGames
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// This is the Lighting.hlsl file for the end of the first video in 2020.2
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color) {
+#if SHADERGRAPH_PREVIEW
+    Direction = float3(0.5, 0.5, 0);
+    Color = 1;
+#else
+    Light mainLight = GetMainLight(0);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+#endif
+}
+
+#endif
