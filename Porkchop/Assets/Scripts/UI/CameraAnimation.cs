a09ce117a7d7eb63efb8d597fed9dc01a00f64e2
jarldn <59234699+jarldn@users.noreply.github.com>
2021-01-15 16:04:50 +0100

    Menu test

---
+++ Porkchop/Assets/Scripts/UI/CameraAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    public void ZoomIn()
+    {
+        animator.SetBool("Zoom", true);
+    }
+
+    public void ZoomOut()
+    {
+        animator.SetBool("Zoom", false);
+    }
+}
---
+++ Porkchop/Assets/Scripts/UI/ClickButtonsAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButtonsAnimation : MonoBehaviour
+{
+    Button[] buttons;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = this.GetComponentsInChildren<Button>();
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].onClick.AddListener(TareaOnClick);
+        }
+        animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void TareaOnClick()
+    {
+        animator.SetTrigger("Mando");
+        Debug.Log("Pulsado");
+    }
+}
