6a5e74a71f8132f363870e92a0c7b341cf8e4c18
jarldn <59234699+jarldn@users.noreply.github.com>
2021-01-19 00:22:03 +0100

    Final point

---
+++ Porkchop/Assets/Scripts/World/FinalLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLevel : MonoBehaviour
+{
+    LevelChanger levelChanger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelChanger = GameObject.Find("Level Changer").GetComponent<LevelChanger>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            levelChanger.fadeToScene(3);
+        }
+    }
+}
